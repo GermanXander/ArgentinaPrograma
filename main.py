f9f00f69a11cc515cd61fc8dc0fd3db9ec7828e8
gax <gax@disroot.org>
2023-03-20 11:17:15 -0300

    dht22 en loop principal

---
+++ main.py
@@ -0,0 +1,38 @@
+from machine import Pin
+import dht
+import time
+import json
+from collections import OrderedDict
+
+sw = Pin(23, Pin.IN)
+led = Pin(2, Pin.OUT)
+d = dht.DHT22(Pin(25))
+print("esperand pulsador")
+contador=0
+
+while True:
+    if sw.value():
+        contador+=1
+        print(contador)
+        if led.value():
+            led.off()
+        else:
+            led.on()
+    try:
+        d.measure()
+        try:
+            temperatura=d.temperature()
+            try:
+                humedad=d.humidity()
+                datos=json.dumps(OrderedDict([
+                    ('temperatura',temperatura),
+                    ('humedad',humedad)
+                ]))
+                print(datos)
+            except OSError as e:
+                print("sin sensor temperatura")
+        except OSError as e:
+            print("sin sensor humedad")
+    except OSError as e:
+        print("sin sensor")
+    time.sleep_ms(150)
