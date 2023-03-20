0c25473db4148ce0b14a2e7e5ccb9adc3a3c616e
gax <gax@disroot.org>
2023-03-20 16:23:47 -0300

    dht22 + timer para pulsador

--- main.py
+++ main.py
@@ -1,23 +1,32 @@
-from machine import Pin
+from machine import Pin, disable_irq, enable_irq, Timer
 import dht
 import time
 import json
 from collections import OrderedDict
+import micropython
 
 sw = Pin(23, Pin.IN)
 led = Pin(2, Pin.OUT)
 d = dht.DHT22(Pin(25))
 print("esperand pulsador")
 contador=0
+estado=False
+
+def alternar(pin):
+    global contador, estado
+    if sw.value():
+        if not estado:
+            contador+=1
+            print(contador)
+            led.value(not led.value())
+        estado = True
+    else:
+        estado = False
+
+timer1 = Timer(1)
+timer1.init(period=50, mode=Timer.PERIODIC, callback=alternar)
 
 while True:
-    if sw.value():
-        contador+=1
-        print(contador)
-        if led.value():
-            led.off()
-        else:
-            led.on()
     try:
         d.measure()
         try:
@@ -35,4 +44,4 @@ while True:
             print("sin sensor humedad")
     except OSError as e:
         print("sin sensor")
-    time.sleep_ms(150)
+    time.sleep(5)
