034f2ff4368d560e86dc704df44159d81f1b8b55
gax <gax@disroot.org>
2023-03-27 16:01:20 -0300

    con contador

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from machine import Pin
 import time
 
+print("esperando pulsador")
+
 sw = Pin(23, Pin.IN)
 led = Pin(2, Pin.OUT)
 
+contador=0
 while True:
     if sw.value():
         led.value(not led.value())
+        contador += 1
+        print(contador)
         time.sleep_ms(5)
