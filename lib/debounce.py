f09b970f39d397c9dcdf9d152d0392ae9e37f929
gax <gax@disroot.org>
2023-04-11 08:40:25 -0300

    usando lib

---
+++ lib/debounce.py
@@ -0,0 +1,29 @@
+import micropython
+from machine import Timer
+timer_init = lambda t, p, cb: t.init(period=p, callback=cb)
+
+class DebouncedSwitch:
+    def __init__(self, sw, cb, arg=None, delay=50, tid=4):
+        self.sw = sw
+        self._sw_cb = self.sw_cb
+        self._tim_cb = self.tim_cb
+        self._set_cb = getattr(self.sw, 'callback', None) or self.sw.irq
+        self.delay = delay
+        self.tim = Timer(tid)
+        self.callback(cb, arg)
+
+    def sw_cb(self, pin=None):
+        self._set_cb(None)
+        timer_init(self.tim, self.delay, self._tim_cb)
+
+    def tim_cb(self, tim):
+        tim.deinit()
+        if self.sw():
+            micropython.schedule(self.cb, self.arg)
+        self._set_cb(self._sw_cb if self.cb else None)
+
+    def callback(self, cb, arg=None):
+        self.tim.deinit()
+        self.cb = cb
+        self.arg = arg
+        self._set_cb(self._sw_cb if cb else None)
--- main.py
+++ main.py
@@ -1,42 +1,15 @@
-import micropython
-from machine import Timer
-timer_init = lambda t, p, cb: t.init(period=p, callback=cb)
-
-class DebouncedSwitch:
-    def __init__(self, sw, cb, arg=None, delay=50, tid=4):
-        self.sw = sw
-        self._sw_cb = self.sw_cb
-        self._tim_cb = self.tim_cb
-        self._set_cb = getattr(self.sw, 'callback', None) or self.sw.irq
-        self.delay = delay
-        self.tim = Timer(tid)
-        self.callback(cb, arg)
-
-    def sw_cb(self, pin=None):
-        self._set_cb(None)
-        timer_init(self.tim, self.delay, self._tim_cb)
-
-    def tim_cb(self, tim):
-        tim.deinit()
-        if self.sw():
-            micropython.schedule(self.cb, self.arg)
-        self._set_cb(self._sw_cb if self.cb else None)
-
-    def callback(self, cb, arg=None):
-        self.tim.deinit()
-        self.cb = cb
-        self.arg = arg
-        self._set_cb(self._sw_cb if cb else None)
-
-
 from machine import Pin
+from debounce import DebouncedSwitch
+
 sw = Pin(23, Pin.IN)
 led = Pin(2, Pin.OUT)
 total=0
+
 def sumador(algo):
     global total
     total+=1
     print(total)
+    led.value(not led.value())
     print(algo)
 
 contador=DebouncedSwitch(sw, sumador)
