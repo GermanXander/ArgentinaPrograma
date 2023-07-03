93f5d4e88036c49bc93eb0108aa13bf9df21432a
Germán Xander <german.xander@fio.unam.edu.ar>
2023-07-03 17:03:03 -0300

    mqtt-comandos

--- main.py
+++ main.py
@@ -4,54 +4,38 @@ import time
 import json
 import ubinascii
 from collections import OrderedDict
-import urequests
-from settings import TOKEN, CHATID, SERVIDOR_MQTT
+from settings import SERVIDOR_MQTT
 from umqtt.robust import MQTTClient
 
 CLIENT_ID = ubinascii.hexlify(unique_id()).decode('utf-8')
 
 mqtt = MQTTClient(CLIENT_ID, SERVIDOR_MQTT,
-                  port=8883, keepalive=10, ssl=True)
+                  port=8883, keepalive=40, ssl=True)
 
 sw = Pin(23, Pin.IN)
 led = Pin(2, Pin.OUT)
 d = dht.DHT22(Pin(25))
 
-print("esperand pulsador")
-contador=0
-estado=False
-datos={}
+def sub_cb(topic, msg):
+    print((topic, msg))
+    if msg==b"apagar":
+        led.value(0)
+    if msg==b"encender":
+        led.value(1)
 
-def alternar(pin):
-    global contador, estado
-    if sw.value():
-        if not estado:
-            contador+=1
-            print(contador)
-            led.value(not led.value())
-            try:
-                data = {'chat_id': CHATID, 'text': datos}
-                response = urequests.post("https://api.telegram.org/bot" + TOKEN + '/sendMessage', json=data)
-                response.close()
-                print("envio correcto a telegram")
-            except:
-                print("fallo en el envio a telegram")
-            estado = True
-        else:
-            estado = False
+mqtt.set_callback(sub_cb)
+mqtt.connect()
+mqtt.subscribe(f"ap/{CLIENT_ID}/comando")
 
 def transmitir(pin):
-    mqtt.connect()
     mqtt.publish(f"ap/{CLIENT_ID}",datos)
-    mqtt.disconnect()
 
 timer1 = Timer(1)
-timer1.init(period=50, mode=Timer.PERIODIC, callback=alternar)
-timer2 = Timer(2)
-timer2.init(period=30000, mode=Timer.PERIODIC, callback=transmitir)
+timer1.init(period=20000, mode=Timer.PERIODIC, callback=transmitir)
+
+datos={}
 
 while True:
-    inicio=time.time_ns()
     try:
         d.measure()
         try:
@@ -69,5 +53,7 @@ while True:
             print("sin sensor humedad")
     except OSError as e:
         print("sin sensor")
+    mqtt.check_msg()
     time.sleep(5)
-    print('duracion: ',(time.time_ns()-inicio)/1000000, ' ms')
+
+mqtt.disconnect()
