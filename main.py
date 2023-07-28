303d6fc5eda01b12d62ce4463cda2784cf0bc75d
Germán Xander <german.xander@fio.unam.edu.ar>
2023-07-28 08:21:27 -0300

    solo un try en dht22

--- main.py
+++ main.py
@@ -20,19 +20,13 @@ while True:
 
     try:
         d.measure()
-        try:
-            temperatura=d.temperature()
-            try:
-                humedad=d.humidity()
-                datos=json.dumps(OrderedDict([
-                    ('temperatura',temperatura),
-                    ('humedad',humedad)
-                ]))
-                print(datos)
-            except OSError as e:
-                print("sin sensor temperatura")
-        except OSError as e:
-            print("sin sensor humedad")
+        temperatura=d.temperature()
+        humedad=d.humidity()
+        datos=json.dumps(OrderedDict([
+            ('temperatura',temperatura),
+            ('humedad',humedad)
+        ]))
+        print(datos)
     except OSError as e:
         print("sin sensor")
 
