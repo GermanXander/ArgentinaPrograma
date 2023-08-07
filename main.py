e60792a4df7ae9ee88f1e8d7864ef4350aad0f0e
Germán Xander <german.xander@fio.unam.edu.ar>
2023-08-07 15:52:42 -0300

    con heartbeat

--- main.py
+++ main.py
@@ -6,8 +6,7 @@ import time
 import json
 import ubinascii
 from collections import OrderedDict
-import urequests
-from settings import TOKEN, CHATID, SERVIDOR_MQTT
+from settings import SERVIDOR_MQTT
 from umqtt.robust import MQTTClient
 
 CLIENT_ID = ubinascii.hexlify(unique_id()).decode('utf-8')
@@ -15,61 +14,40 @@ CLIENT_ID = ubinascii.hexlify(unique_id()).decode('utf-8')
 mqtt = MQTTClient(CLIENT_ID, SERVIDOR_MQTT,
                   port=8883, keepalive=10, ssl=True)
 
-sw = Pin(23, Pin.IN)
 led = Pin(2, Pin.OUT)
 d = dht.DHT22(Pin(25))
+contador = 0
 
-print("esperand pulsador")
-contador=0
-estado=False
-datos={}
-
-def alternar(pin):
-    global contador, estado
-    if sw.value():
-        if not estado:
-            contador+=1
-            print(contador)
-            led.value(not led.value())
-            try:
-                data = {'chat_id': CHATID, 'text': datos}
-                response = urequests.post("https://api.telegram.org/bot" + TOKEN + '/sendMessage', json=data)
-                response.close()
-                print("envio correcto a telegram")
-            except:
-                print("fallo en el envio a telegram")
-            estado = True
-        else:
-            estado = False
-
+def heartbeat(nada):
+    global contador
+    if contador > 5:
+        pulsos.deinit()
+        contador = 0
+        return
+    led.value(not led.value())
+    contador += 1
+  
 def transmitir(pin):
+    print("publicando")
     mqtt.connect()
     mqtt.publish(f"ap/{CLIENT_ID}",datos)
     mqtt.disconnect()
+    pulsos.init(period=150, mode=Timer.PERIODIC, callback=heartbeat)
 
-timer1 = Timer(1)
-timer1.init(period=50, mode=Timer.PERIODIC, callback=alternar)
-timer2 = Timer(2)
-timer2.init(period=30000, mode=Timer.PERIODIC, callback=transmitir)
+publicar = Timer(0)
+publicar.init(period=30000, mode=Timer.PERIODIC, callback=transmitir)
+pulsos = Timer(1)
 
 while True:
-    inicio=time.time_ns()
     try:
         d.measure()
-        try:
-            temperatura=d.temperature()
-            try:
-                humedad=d.humidity()
-                datos=json.dumps(OrderedDict([
-                    ('temperatura',temperatura),
-                    ('humedad',humedad)
-                ]))
-                print(datos)
-            except OSError as e:
-                print("sin sensor temperatura")
-        except OSError as e:
-            print("sin sensor humedad")
+        temperatura = d.temperature()
+        humedad = d.humidity()
+        datos = json.dumps(OrderedDict([
+            ('temperatura',temperatura),
+            ('humedad',humedad)
+        ]))
+        print(datos)
     except OSError as e:
         print("sin sensor")
     time.sleep(5)
-    print('duracion: ',(time.time_ns()-inicio)/1000000, ' ms')
